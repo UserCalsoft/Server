897759b92ca0f9c0f43b81046ac860dfe0aa560a
UserCalsoft <snehal.turambekar@CALSOFTHQ.COM>
2016-05-23 19:12:59 +0530

    Update with new features

--- config.js
+++ config.js
@@ -2,12 +2,15 @@
 
 var Server_Port = 21;
 var Server_IP = 'Enter IP';
-var File_List_Count_Limit = 20;
+var File_List_Count_Limit = 100;
 var Region ='us-east-1';
-var S3_Endpoint ='https://s3.amazonaws.com';
+var S3_Server_EndPoint ="https://s3.amazonaws.com";
+
 
 exports.ServerPort = Server_Port;
 exports.ServerIP = Server_IP;
+exports.AccessKeyId = Access_Key_Id;
+exports.SecretKey = Secret_Key;
 exports.Region = Region;
 exports.FileListCountLimit = File_List_Count_Limit;
-exports.S3Endpoint = S3_Endpoint;
+exports.S3ServerEndPoint = S3_Server_EndPoint;
